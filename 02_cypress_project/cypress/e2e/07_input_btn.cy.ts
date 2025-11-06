7d7afd340f6f515bf329ee8eae1d2569f2a5679f
David Kalmus <dejvios@seznam.cz>
2025-11-06 09:50:29 +0100

    cypress

--- 02_cypress_project/cypress/e2e/07_input_btn.cy.ts
+++ 02_cypress_project/cypress/e2e/07_input_btn.cy.ts
@@ -8,5 +8,20 @@ describe('Scénář', () => {
         cy.get('input.form-control');
         cy.get('#newButtonName');
         cy.get('input#newButtonName');
+
+        // cy.get('input#newButtonName').type('David {home} Dejv 17');
+        cy.get('input#newButtonName').type('David Dejv 17');
+
+        cy.get('button#updatingButton')
+            .click()
+            .should('have.text', 'David Dejv 17');
+        
+        cy.get('button#updatingButton')
+            .click()
+            .then(btn => {
+                // btn - jQuery objekt
+                expect(btn).to.have.text('David Dejv 17'); // expect - jQuery objekt, Cypress chainable objekt
+                // btn.should('have.text', 'David Dejv 17') // should - Cypress chainable objekt
+            });
     })
-})
+})     
